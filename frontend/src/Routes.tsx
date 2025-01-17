ce3fad48d07495e32bd8dfbe99a2eb9c2550c762
Bernard Assogba <montfortassogba2@gmail.com>
2025-01-17 11:19:49 +0200

    .

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/home';
 import P404 from './components/404page';
 import SiteManagement from './components/siteMnagement/main';
 import LandingPage from './components/LandingPage';

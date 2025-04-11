af91e5f6550ff5dcf1feb07a0f09230c7e836f63
bernar-As <montfortassogba2@gmial.com>
2025-04-11 19:53:47 +0300

    .

--- frontend/src/components/MyTimeTable/index.jsx
+++ frontend/src/components/MyTimeTable/index.jsx
@@ -17,7 +17,7 @@ const MyTimetable = ()=>{
     const [tableMobileData,settableMobileData] = useState([])
     const [mobileColumns,setMobileColumns] = useState()
     const [marks,setMarks] = useState()
-    const [selectedScheduleType,setselectedScheduleType] = useState('All')
+    const [selectedScheduleType,setselectedScheduleType] = useState('3')
     const [scheduleType,setscheduleType] = useState([])
     const [columns, setColumns] = useState([]) 
     const [columns1, setColumns1] = useState([]) 

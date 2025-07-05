24468b937000a6c13cb798128e2e12bc80a88987
bernard-As <montfortassogba2@gmail.com>
2025-07-05 10:56:51 +0300

    .

---
+++ frontend/src/components/CourseSelection/index.jsx
@@ -0,0 +1,11 @@
+const CourseSelection = () => {
+  return (
+    <div className="course-selection">
+      <h2>Select Your Courses</h2>
+      <p>Choose from a variety of courses to enhance your learning experience.</p>
+      {/* Course selection logic will go here */}
+    </div>
+  );
+}
+
+export default CourseSelection;
